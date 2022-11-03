fac388be933236b229e476e020915e65aac6f544
Prisana Cheng <chengprisana@gmail.com>
2022-11-03 15:38:05 -0500

    set up boiler plate for react and created component files

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+        <h1 id="mainHeadline">P<sup>2</sup></h1>
+        <h3>Saturday, November 11th, 2023</h3>
+        <h4>Countdown component will go here</h4>
+      <header>
+        Links go here: Home, Photos, Wedding Party, Accomodation, QandA,
+        Registry, RSVP
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {App} from './components';
+
+const container = document.getElementById('app')
+const root = ReactDOM.createRoot(container)
+
+root.render(<App />)
